9ef3e4bbc198fd9835de1a3b30a2f925e6448015
NikolausA <nik.aleksenko@gmail.com>
2020-09-09 20:15:51 +0300

    добавил миграцию таблицы новостей, категории

---
+++ database/seeds/NewsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('news')->insert($this->getData());
+    }
+
+    public function getData(): array {
+        $data = [];
+        $faker = Faker\Factory::create('ru_RU');
+        for ($i=0; $i < 10; $i++) {
+            $data[] = [
+                'title' => $faker->realText(rand(10, 30)),
+                'text' => $faker->realText(rand(10, 200)),
+                'isPrivate' => (boolean)rand(0, 1),
+            ];
+        }
+        return $data;
+    }
+}
